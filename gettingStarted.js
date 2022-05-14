094a8ae8c99e88d34b0458a22d1875d38ebef104
arun9718 <arunavasahachowdhury@gmail.com>
2022-05-14 14:18:14 +0530

    value definition

--- gettingStarted.js
+++ gettingStarted.js
@@ -1,1 +1,9 @@
 console.log("Hello JS");
+
+//Java is a statically typed language
+//It needs variable and its type to declare a variable
+//while in javascript we just need to declare a variable
+
+int a;
+
+let a;//default value is undefined
