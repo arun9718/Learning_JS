dccf00b5077844009a7d7b298359a8192c4c03ea
arun9718 <arunavasahachowdhury@gmail.com>
2022-06-11 01:00:29 +0530

    ec start

--- js_Interview.js
+++ js_Interview.js
@@ -9,3 +9,13 @@ console.log(process.cwd());//same as above
 
 console.log(path.extname(__filename));
 console.log(path.basename(__filename));
+
+//Execution Context
+//global and this
+
+//whenver a  js code is executed u will get a global variable and this variables
+console.log("global\n",global);
+console.log("this\n",this);
+
+//the code that is not inside any function falls in the global area
+//it is in the default execution context 
