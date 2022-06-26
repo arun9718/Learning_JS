b7500f8495d3455a2bba3fad84301dd8452619c2
arun9718 <arunavasahachowdhury@gmail.com>
2022-06-26 12:38:52 +0530

    creating element in DOm

--- Browser/script.js
+++ Browser/script.js
@@ -21,3 +21,22 @@ console.log("line number 19", text);
 let anchorElem = document.querySelector("a");
 let output = anchorElem.getAttribute("href");
 console.log("line number 23", output)
+
+
+//create p tag
+let pelem=document.createElement("p");
+//put content
+pelem.textContent="Hello DOM";
+pelem.style.backgroundColor = "lightgreen";
+
+//copy pelem
+let newelem=pelem.cloneNode(true);
+newelem.textContent="Appemd Child";
+
+//placing the elements in the DOM tree
+let body= document.querySelector("body");
+//place at last
+body.appendChild(newelem);
+
+//place at before of some elements
+body.insertBefore(pelem,elem);
