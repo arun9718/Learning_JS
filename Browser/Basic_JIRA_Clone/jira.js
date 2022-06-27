1500958a239aa252ebad3ef62a10fd19e633fd32
arun9718 <arunavasahachowdhury@gmail.com>
2022-06-28 02:02:31 +0530

    completed till task selection

---
+++ Browser/Basic_JIRA_Clone/jira.js
@@ -0,0 +1,13 @@
+let colors = document.querySelectorAll(".color");
+let main = document.querySelector(".main");
+for(let i=0;i<colors.length;i++){
+  colors[i].addEventListener("click",changeColor);
+  function changeColor(e){
+    let elem = e.currentTarget;
+    let classes = elem.classList;
+    let color = classes[1];
+    main.style.backgroundColor =color;
+    console.log(elem);
+
+  }
+}
