7c127f27037a1807f3a03b1a2213bf645141c9f8
arun9718 <arunavasahachowdhury@gmail.com>
2022-07-02 01:31:03 +0530

    Completed till random ticket id generation and color priority change of each ticket

--- Browser/Basic_JIRA_Clone/jira.js
+++ Browser/Basic_JIRA_Clone/jira.js
@@ -1,7 +1,8 @@
 const colors = document.querySelectorAll(".color");
 const main = document.querySelector(".main");
 const addBtn = document.querySelector(".add");
-for(let i=0;i<colors.length;i++){
+const color = ["pink","blue","green","black"];
+/*for(let i=0;i<colors.length;i++){
   colors[i].addEventListener("click",changeColor);
   function changeColor(e){
     let elem = e.currentTarget;
@@ -12,20 +13,51 @@ for(let i=0;i<colors.length;i++){
 
 
   }
-}
+}*/
 
-addBtn.addEventListener("click",createTicket);
+addBtn.addEventListener("click",function () {
+    // create a ticket
+    createTicket();
+});
 function createTicket(){
+  let id = uuidv4();
   let ticket = document.createElement("div");
   ticket.setAttribute("class","ticket");
   ticket.innerHTML=`
-  <div class="ticket-header">
+  <div class="ticket-header black">
   </div>
   <div class="ticket-content">
     <div class="rand-id">
-      #QES4FGG
+      #${id}
     </div>
     <textarea name="name" class="text-area"></textarea>
   </div>`
   main.appendChild(ticket);
+
+  let header = document.querySelector(".ticket-header");
+  header.addEventListener("click",changeColor);
+
+
+}
+function changeColor(e){
+  let header = e.currentTarget;
+  let classes = header.classList;
+  let curr_color = classes[1];
+
+  let currIdx=0;
+  for(let i=0;i,color.length;i++){
+    if(curr_color==color[i])
+    {
+      currIdx=i;
+      break;
+    }
+  }
+
+  let nextIdx=(currIdx+1)%color.length;
+  let nextColor = color[nextIdx];
+
+  classes.remove(curr_color);
+  classes.add(nextColor);
+
+
 }
