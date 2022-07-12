8dc478eefd1e29853c970ac3d79783c62869789a
arun9718 <arunavasahachowdhury@gmail.com>
2022-07-12 20:48:07 +0530

    added menu bar items

--- Excel_Clone/js/script.js
+++ Excel_Clone/js/script.js
@@ -1,5 +1,6 @@
 let topRow = document.querySelector(".top_row");
 let leftCol = document.querySelector(".left_col");
+let grid = document.querySelector(".grid");
 
 
 //left column
@@ -17,4 +18,20 @@ for(let i=1;i<=26;i++){
   div.setAttribute("class","cell");
   div.textContent = String.fromCharCode(i+64);
   topRow.appendChild(div);
+}
+
+//generate grid
+for(let i=1;i<=100;i++){
+  let row = document.createElement("div");
+  row.setAttribute("class","row");
+  for(let j=1;j<=26;j++){
+    let cell = document.createElement("div");
+    cell.setAttribute("class","cell");
+    cell.setAttribute("rid",i);
+    cell.setAttribute("cid",j);
+    row.appendChild(cell);
+    console.log(i,j);
+  }
+  grid.appendChild(row);
+
 }
